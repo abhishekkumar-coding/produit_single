26f2fdb4a841766e682c7406f4dc182d06be5733
abhishek kumar <abhiwebdev2.0@gmail.com>
2024-12-08 18:54:03 -0800

    hero component updated

--- components/hero.jsx
+++ components/hero.jsx
@@ -25,10 +25,10 @@ const ProductDetails = () => (
   <div>
     <div className="border-b flex items-center justify-between">
       <div className="border-b">
-        <h1 className="text-[30px]">Cheese – appareil à raclette 1/2 roue</h1>
+        <h1 className="text-[30px]">Cheese – appareil &agrave; raclette 1/2 roue</h1>
         <span className="flex items-center gap-3">
-          <h1 className="text-[30px]">39,50€</h1>
-          <p className="text-gray-600 text-[14px]">/pièce</p>
+          <h1 className="text-[30px]">39,50&eur;</h1>
+          <p className="text-gray-600 text-[14px]">/pi&egrave;ce</p>
         </span>
       </div>
       <Image
@@ -41,25 +41,25 @@ const ProductDetails = () => (
     <div className="flex border-b items-center justify-between px-5">
       <div className="flex items-center gap-8">
         <span className="flex items-center gap-4 py-6">
-          <Image src={scale} alt=""/>
+          <Image src={scale} alt="Scale Icon" />
           <p>
             20<sup>cm</sup>
           </p>
         </span>
         <span className="flex items-center gap-4 py-6">
-          <Image src={circle} alt=""/>
+          <Image src={circle} alt="Circle Icon" />
           <p>
             50<sup>cm</sup>
           </p>
         </span>
       </div>
-      <span>RÉF : VABGN5</span>
+      <span>R&Eacute;F : VABGN5</span>
     </div>
     <div className="py-8 border-b h-[390px]">
       <p className="w-[60%]">
-        Location appareil à raclette - Raclette traditionnelle 1/2 roue Réglable
-        en hauteur Appareil à raclette professionnel Boîtier de chauffe
-        horizontal réglable en hauteur <br />
+        Location appareil &agrave; raclette - Raclette traditionnelle 1/2 roue
+        R&eacute;glable en hauteur Appareil &agrave; raclette professionnel
+        Bo&icirc;tier de chauffe horizontal r&eacute;glable en hauteur <br />
         <br /> 220V <br /> 900W
       </p>
     </div>
@@ -85,42 +85,43 @@ function Hero() {
         <h2 className="text-gray-500">Art de la table</h2>
       </div>
 
-      {/* HERO SECTIONS DIV  */}
+      {/* HERO SECTIONS DIV */}
       <div className="flex items-center justify-around">
-        {/* LEFT SIDE DIV  */}
+        {/* LEFT SIDE DIV */}
         <div>
           <div className="relative flex flex-col items-center justify-center w-[760px] h-[650px] bg-[#F8F6F4] rounded-3xl">
             <ProductImages />
-            <div className="">
+            <div>
               <Image src={machine} alt="Machine" />
             </div>
           </div>
           <div className="w-[740px] mt-10">
             <h1 className="text-3xl my-5">Description produit</h1>
             <p className="text-[1vw]">
-              Festi vous propose à la location un/une "Jewel – grand
-              couteau/10pc" pour votre évenement et ce dès 0,35 € / pièce
-              (HTVA). Que ce soit pour votre mariage, une fête d'anniversaire ou
-              du personnel, ce produit a fait l'objet d'une sélection rigoureuse
-              par notre équipe. Il est en location chez nous sous la référence
-              "VAJGC". Nous sommes à votre disposition pour que les événements
-              de nos clients, même en last-minute, soient toujours une réussite.
-              Vous pourrez trouver tout une série d'autres produits à louer de
-              ce type dans la catégorie "Art de la Table".
+              Festi vous propose &agrave; la location un/une &quot;Jewel – grand
+              couteau/10pc&quot; pour votre &eacute;venement et ce d&egrave;s 0,35 &euro; /
+              pi&egrave;ce (HTVA). Que ce soit pour votre mariage, une f&ecirc;te
+              d&apos;anniversaire ou du personnel, ce produit a fait l&apos;objet
+              d&apos;une s&eacute;lection rigoureuse par notre &eacute;quipe. Il est en
+              location chez nous sous la r&eacute;f&eacute;rence &quot;VAJGC&quot;. Nous sommes
+              &agrave; votre disposition pour que les &eacute;v&eacute;nements de nos
+              clients, m&ecirc;me en last-minute, soient toujours une r&eacute;ussite.
+              Vous pourrez trouver tout une s&eacute;rie d&apos;autres produits &agrave; louer
+              de ce type dans la cat&eacute;gorie &quot;Art de la Table&quot;.
             </p>
           </div>
         </div>
 
-        {/* RIGHT SIDE DIV  */}
-        <div className="">
+        {/* RIGHT SIDE DIV */}
+        <div>
           <div className="relative w-[760px] h-[650px]">
             <ProductDetails />
           </div>
           <div className="flex flex-col mt-16">
-            <button className="bg-[#FBF9F8] TEXT-[16px] p-4 my-1 text-start w-[758px]">
+            <button className="bg-[#FBF9F8] text-[16px] p-4 my-1 text-start w-[758px]">
               LIVRAISONS
             </button>
-            <button className="bg-[#FBF9F8] TEXT-[16px] p-4 my-1 text-start w-[758px]">
+            <button className="bg-[#FBF9F8] text-[16px] p-4 my-1 text-start w-[758px]">
               QUESTIONS
             </button>
           </div>
